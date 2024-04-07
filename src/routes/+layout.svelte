8b68e2094d0d98099575e6582bd7c9c4ca29208c
fruit-bird <github.ru3w9@slmail.me>
2024-04-07 18:37:51 +0000

    added mock floating navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,17 @@
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
 	import ModeToggle from '$lib/components/ui/ModeToggle.svelte';
+	import { FloatingNavbar } from '$lib/components/ui/floating-navbar';
+	import { Settings } from 'lucide-svelte'
+
+	const navItems = [
+		{ name: 'Home', link: '/' },
+		{ name: 'About', link: '/about' },
+		{ name: 'Settings', link: '/settings', icon: Settings }
+	];
 </script>
 
-<nav>
-	<ul>
-		<li><a href="/">Home</a></li>
-		<li><a href="/about">About</a></li>
-		<li><a href="/settings">Settings</a></li>
-	</ul>
-</nav>
+<FloatingNavbar {navItems} />
 
 <ModeWatcher />
 <ModeToggle />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
-	import ModeToggle from '$lib/components/ui/ModeToggle.svelte';
-	import { ScrollArea } from '$lib/components/ui/scroll-area';
-
-    import Login from '$lib/Login.svelte';
+	import Login from '$lib/Login.svelte';
 	import Messages from '$lib/Messages.svelte';
 	import { currentUser } from '$lib/pocketbase';
 </script>
