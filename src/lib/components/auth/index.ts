c93c10f4c74f339fe710ead9b185ba48c3436e2f
fruit-bird <github.ru3w9@slmail.me>
2024-04-12 18:39:28 +0000

    grouped auth components

---
+++ src/lib/components/auth/index.ts
@@ -0,0 +1,9 @@
+import LoginDialog from './LoginDialog.svelte';
+import SignUpDialog from './SignUpDialog.svelte';
+import SignOutDialog from './SignOutDialog.svelte';
+
+export {
+    LoginDialog,
+    SignUpDialog,
+    SignOutDialog
+};
