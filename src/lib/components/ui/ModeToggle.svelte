8919e941d6a45aeb7a6ae5d3343b7e54050f726e
fruit-bird <github.ru3w9@slmail.me>
2024-04-18 19:22:29 +0100

    exported classes for the component

--- src/lib/components/ui/ModeToggle.svelte
+++ src/lib/components/ui/ModeToggle.svelte
@@ -4,11 +4,14 @@
 
 	import { Button } from '$lib/components/ui/button';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
 </script>
 
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger asChild let:builder>
-		<Button builders={[builder]} variant="outline" size="icon">
+		<Button builders={[builder]} variant="outline" size="icon" class={className}>
 			<Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
 			<Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
 			<span class="sr-only">Toggle theme</span>
