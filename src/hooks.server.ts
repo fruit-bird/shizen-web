d315c64f8e178af4ae60398a8ffbd7dae1421e50
fruit-bird <github.ru3w9@slmail.me>
2024-05-04 12:37:22 +0100

    fixed hook to store user in cookie

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,19 +2,18 @@ import PocketBase from 'pocketbase';
 import { serializeNonPOJOs } from '$lib/utils';
 import type { Handle } from '@sveltejs/kit';
 
-// idk why errors persist, the code works fine
 export const handle: Handle = async ({ event, resolve }) => {
     event.locals.pb = new PocketBase("http://127.0.0.1:8090");
     event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
     if (event.locals.pb.authStore.isValid) {
-        event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model as object);
+        event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model);
     } else {
         event.locals.user = undefined;
     }
 
     const response = await resolve(event);
-    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie());
+    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }));
 
     return response;
 }
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,3 @@
 export const load = async ({ locals }) => {
-    if (locals.user) {
-        return {
-            user: locals.user
-        }
-    }
-    return {
-        user: undefined
-    }
+    return { user: locals.user }
 }
