11cf5250d4899728f6b1ad98e33384dba9a8cba2
fruit-bird <github.ru3w9@slmail.me>
2024-05-05 14:21:11 +0100

    logo image changes with theme

--- src/lib/components/nav/NavBar.svelte
+++ src/lib/components/nav/NavBar.svelte
@@ -4,6 +4,7 @@
 	import UserNav from './UserNav.svelte';
 	// import TeamSwitcher from './TeamSwitcher.svelte';
 	import type { AuthModel } from 'pocketbase';
+	import { mode } from 'mode-watcher';
 
 	export let navItems: { title: string; href: string }[] = [];
 	export let user: AuthModel | undefined;
@@ -14,9 +15,13 @@
 		<div class="flex h-16 items-center px-4">
 			<!-- <TeamSwitcher /> -->
 			<a href="/" class="flex items-center space-x-2 pl-8">
-				<!-- <img src="/images/shizen_light.svg" alt="Shizen" class="h-8 w-8 rounded-full" /> -->
-				<img src="/images/shizen_dark.png" alt="Shizen" class="h-8 w-8 rounded-full" />
-				<span class="text-lg font-extrabold pl-1">Shizen</span>
+				<!-- workaround for now -->
+				{#if $mode === 'dark'}
+					<img src="/images/shizen_dark.png" alt="Shizen" class="h-8 w-8 rounded-full" />
+				{:else}
+					<img src="/images/shizen_light.png" alt="Shizen" class="h-8 w-8 rounded-full" />
+				{/if}
+				<span class="pl-1 text-lg font-extrabold">Shizen</span>
 			</a>
 			<div class="flex flex-grow items-center justify-center">
 				<MainNav {navItems} />
